744dd1568adfab4254d99c0fb0f07dec82407d14
M. A. Kowalski <mak60@cam.ac.uk>
2026-03-02 11:35:15 +0000

    doc: say that csd3 scripts must be run from the root of the repo

--- scripts/run-ncu-csd3.sh
+++ scripts/run-ncu-csd3.sh
@@ -5,6 +5,10 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --time=02:00:00
+#
+# NOTE: This script must be run from the root of the repository!
+#       It relies on some hardcoded relative paths.
+#       e.g. submit with `sbatch scripts/run-ncu-csd3.sh`
 
 set -euo pipefail
 
--- scripts/run-nsys-csd3.sh
+++ scripts/run-nsys-csd3.sh
@@ -5,6 +5,12 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --time=01:00:00
+#
+# NOTE: This script must be run from the root of the repository!
+#       It relies on some hardcoded relative paths.
+#       e.g. submit with `sbatch scripts/run-ncu-csd3.sh`
+
+
 
 set -euo pipefail
 
