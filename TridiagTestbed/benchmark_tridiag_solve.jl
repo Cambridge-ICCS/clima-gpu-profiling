816d79961ab6138f69b97c60f5be98b217f44564
M. A. Kowalski <mak60@cam.ac.uk>
2026-03-02 11:04:29 +0000

    refactor: move tridiagonal solver benchmark to seperate project

--- TridiagTestbed/benchmark_tridiag_solve.jl
+++ TridiagTestbed/benchmark_tridiag_solve.jl
@@ -1,20 +1,26 @@
 """Benchmark tests for FieldMatrix tridiagonal solves.
 
-Run with:
-  clear && julia --project=ClimaCore.jl-mod/.buildkite scripts/benchmark_tridiag_solve.jl
 """
+
+using Pkg
+
 
 # Figure out which project is currently activated
 project_dir = dirname(Base.active_project())
 @info "Active project: $project_dir"
 
-if contains(project_dir, "-mod")
-    package_dir = "ClimaCore.jl-mod"
-else
-    package_dir = "ClimaCore.jl"
+
+const clima_core_path = begin
+    uuid = get(
+        () -> error("`ClimaCore` not found in the project dependencies."),
+        Pkg.project().dependencies,
+        "ClimaCore",
+    )
+    abspath(Pkg.dependencies()[uuid].source)
 end
 
-include(joinpath("..", package_dir, "test", "MatrixFields", "matrix_field_test_utils.jl"))
+
+include(joinpath(clima_core_path, "test", "MatrixFields", "matrix_field_test_utils.jl"))
 
 # Generate extruded finite difference spaces for testing. Include topography
 # when possible.
@@ -32,9 +38,9 @@ function test_spaces(::Type{FT}) where {FT}
     vdomain = Domains.IntervalDomain(
         Geometry.ZPoint(FT(0)),
         Geometry.ZPoint(FT(10));
-        boundary_names=(:bottom, :top),
+        boundary_names = (:bottom, :top),
     )
-    vmesh = Meshes.IntervalMesh(vdomain, nelems=velem)
+    vmesh = Meshes.IntervalMesh(vdomain, nelems = velem)
     vtopology = Topologies.IntervalTopology(comms_ctx, vmesh)
     vspace = Spaces.CenterFiniteDifferenceSpace(vtopology)
     sfc_coord = Fields.coordinate_field(hspace)
@@ -43,8 +49,7 @@ function test_spaces(::Type{FT}) where {FT}
         Hypsography.LinearAdaption(
             Geometry.ZPoint.(@. cosd(sfc_coord.lat) + cosd(sfc_coord.long) + 1),
         ) # TODO: FD operators don't currently work with hypsography on GPUs.
-    center_space =
-        Spaces.ExtrudedFiniteDifferenceSpace(hspace, vspace, hypsography)
+    center_space = Spaces.ExtrudedFiniteDifferenceSpace(hspace, vspace, hypsography)
     face_space = Spaces.FaceExtrudedFiniteDifferenceSpace(center_space)
 
     return center_space, face_space
@@ -55,8 +60,8 @@ end
 # also returns corresponding FieldVector
 function dycore_prognostic_EDMF_FieldMatrix(
     ::Type{FT},
-    center_space=nothing,
-    face_space=nothing,
+    center_space = nothing,
+    face_space = nothing,
 ) where {FT}
     seed!(1) # For reproducibility with random fields
     if isnothing(center_space) || isnothing(face_space)
@@ -79,9 +84,8 @@ function dycore_prognostic_EDMF_FieldMatrix(
     e³ = Geometry.Covariant3Vector(1)
     e₃ = Geometry.Contravariant3Vector(1)
 
-    ρχ_unit = (; ρq_tot=1, ρq_liq=1, ρq_ice=1, ρq_rai=1, ρq_sno=1)
-    ρaχ_unit =
-        (; ρaq_tot=1, ρaq_liq=1, ρaq_ice=1, ρaq_rai=1, ρaq_sno=1)
+    ρχ_unit = (; ρq_tot = 1, ρq_liq = 1, ρq_ice = 1, ρq_rai = 1, ρq_sno = 1)
+    ρaχ_unit = (; ρaq_tot = 1, ρaq_liq = 1, ρaq_ice = 1, ρaq_rai = 1, ρaq_sno = 1)
 
     ᶠᶜmat2_u₃_scalar = ᶠᶜmat2 .* (e³,)
     ᶜᶠmat2_scalar_u₃ = ᶜᶠmat2 .* (e₃',)
@@ -90,24 +94,22 @@ function dycore_prognostic_EDMF_FieldMatrix(
     ᶜᶜmat3_uₕ_scalar = ᶜᶜmat3 .* (e¹²,)
     ᶜᶜmat3_uₕ_uₕ =
         ᶜᶜmat3 .* (
-            Geometry.Covariant12Vector(1, 0) *
-            Geometry.Contravariant12Vector(1, 0)' +
-            Geometry.Covariant12Vector(0, 1) *
-            Geometry.Contravariant12Vector(0, 1)',
+            Geometry.Covariant12Vector(1, 0) * Geometry.Contravariant12Vector(1, 0)' +
+            Geometry.Covariant12Vector(0, 1) * Geometry.Contravariant12Vector(0, 1)',
         )
     ᶜᶠmat2_uₕ_u₃ = ᶜᶠmat2 .* (e¹² * e₃',)
     ᶜᶜmat3_ρχ_scalar = map(Base.Fix1(map, Base.Fix2(⊠, ρχ_unit)), ᶜᶜmat3)
     ᶜᶜmat3_ρaχ_scalar = map(Base.Fix1(map, Base.Fix2(⊠, ρaχ_unit)), ᶜᶜmat3)
     ᶜᶠmat2_ρaχ_u₃ = map(Base.Fix1(map, Base.Fix2(⊠, ρaχ_unit ⊠ e₃')), ᶜᶠmat2)
 
-    dry_center_gs_unit = (; ρ=1, ρe_tot=1, uₕ=e¹²)
-    center_gs_unit = (; dry_center_gs_unit..., ρatke=1, ρχ=ρχ_unit)
-    center_sgsʲ_unit = (; ρa=1, ρae_tot=1, ρaχ=ρaχ_unit)
+    dry_center_gs_unit = (; ρ = 1, ρe_tot = 1, uₕ = e¹²)
+    center_gs_unit = (; dry_center_gs_unit..., ρatke = 1, ρχ = ρχ_unit)
+    center_sgsʲ_unit = (; ρa = 1, ρae_tot = 1, ρaχ = ρaχ_unit)
 
     b = Fields.FieldVector(;
-        sfc=sfc_vec .* ((; T=1),),
-        c=ᶜvec .* ((; center_gs_unit..., sgsʲs=(center_sgsʲ_unit,)),),
-        f=ᶠvec .* ((; u₃=e³, sgsʲs=((; u₃=e³),)),),
+        sfc = sfc_vec .* ((; T = 1),),
+        c = ᶜvec .* ((; center_gs_unit..., sgsʲs = (center_sgsʲ_unit,)),),
+        f = ᶠvec .* ((; u₃ = e³, sgsʲs = ((; u₃ = e³),)),),
     )
     A = MatrixFields.FieldMatrix(
         # GS-GS blocks:
@@ -135,28 +137,26 @@ function dycore_prognostic_EDMF_FieldMatrix(
 end
 
 
-function test_field_matrix_solver(; test_name, alg, A, b, use_rel_error=false)
+function test_field_matrix_solver(; test_name, alg, A, b, use_rel_error = false)
     # @testset "$test_name" begin
     x = similar(b)
     A′ = FieldMatrixWithSolver(A, b, alg)
     @test zero(A′) isa typeof(A′)
-    solve_time =
-        @benchmark ClimaComms.@cuda_sync comms_device ldiv!(x, A′, b)
+    solve_time = @benchmark ClimaComms.@cuda_sync comms_device ldiv!(x, A′, b)
 
     b_test = similar(b)
     # @test zero(b) isa typeof(b)
-    mul_time =
-        @benchmark ClimaComms.@cuda_sync comms_device mul!(b_test, A′, x)
+    mul_time = @benchmark ClimaComms.@cuda_sync comms_device mul!(b_test, A′, x)
 
-    solve_time_rounded = round(solve_time; sigdigits=2)
-    mul_time_rounded = round(mul_time; sigdigits=2)
+    solve_time_rounded = round(solve_time; sigdigits = 2)
+    mul_time_rounded = round(mul_time; sigdigits = 2)
     time_ratio = solve_time_rounded / mul_time_rounded
-    time_ratio_rounded = round(time_ratio; sigdigits=2)
+    time_ratio_rounded = round(time_ratio; sigdigits = 2)
 
     error_vector = abs.(parent(b_test) .- parent(b))
     if use_rel_error
         rel_error = norm(error_vector) / norm(parent(b))
-        rel_error_rounded = round(rel_error; sigdigits=2)
+        rel_error_rounded = round(rel_error; sigdigits = 2)
         error_string = "Relative Error = $rel_error_rounded"
     else
         max_error = maximum(error_vector)
@@ -203,10 +203,10 @@ for (vector, matrix, string1, string2) in (
     (ᶠvec, ᶠᶠmat3, "tri-diagonal matrix", "cell faces"),
 )
     test_field_matrix_solver(;
-        test_name="$string1 solve on $string2",
-        alg=MatrixFields.BlockDiagonalSolve(),
-        A=MatrixFields.FieldMatrix((@name(_), @name(_)) => matrix),
-        b=Fields.FieldVector(; _=vector),
+        test_name = "$string1 solve on $string2",
+        alg = MatrixFields.BlockDiagonalSolve(),
+        A = MatrixFields.FieldMatrix((@name(_), @name(_)) => matrix),
+        b = Fields.FieldVector(; _ = vector),
     )
 end
 
@@ -219,28 +219,26 @@ keyname1 = @name(var1)
 
 A1 = MatrixFields.FieldMatrix((keyname1, keyname1) => A[keyname])
 b1_entry = MatrixFields.get_field(b, keyname[1])
-b1 = Fields.FieldVector(; var1=b1_entry)
+b1 = Fields.FieldVector(; var1 = b1_entry)
 
 test_field_matrix_solver(;
-    test_name="Dycore + prognostic, EDMF + prognostic surface temperature \
-               solve",
-    alg=MatrixFields.BlockDiagonalSolve(),
-    A=A1,
-    b=b1,
+    test_name = "Dycore + prognostic, EDMF + prognostic surface temperature \
+                 solve",
+    alg = MatrixFields.BlockDiagonalSolve(),
+    A = A1,
+    b = b1,
 )
-# end
+
 
 # Test batched tri-diagonal solver
-if package_dir == "ClimaCore.jl-mod"
-    for (vector, matrix, string1, string2) in (
-        (ᶜvec, ᶜᶜmat3, "tri-diagonal matrix", "cell centers"),
-        (ᶠvec, ᶠᶠmat3, "tri-diagonal matrix", "cell faces"),
+for (vector, matrix, string1, string2) in (
+    (ᶜvec, ᶜᶜmat3, "tri-diagonal matrix", "cell centers"),
+    (ᶠvec, ᶠᶠmat3, "tri-diagonal matrix", "cell faces"),
+)
+    test_field_matrix_solver(;
+        test_name = "Batched $string1 solve on $string2",
+        alg = MatrixFields.BatchedTridiagonalSolve(),
+        A = MatrixFields.FieldMatrix((@name(_), @name(_)) => matrix),
+        b = Fields.FieldVector(; _ = vector),
     )
-        test_field_matrix_solver(;
-            test_name="Batched $string1 solve on $string2",
-            alg=MatrixFields.BatchedTridiagonalSolve(),
-            A=MatrixFields.FieldMatrix((@name(_), @name(_)) => matrix),
-            b=Fields.FieldVector(; _=vector),
-        )
-    end
 end
